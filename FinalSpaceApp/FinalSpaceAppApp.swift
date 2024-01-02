42cda6bcb41239d8fc1da3c3b726f7c49ec00bff
Bram Lippens <Bram>
2024-01-02 14:08:03 +0100

    Initial Commit

---
+++ FinalSpaceApp/FinalSpaceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalSpaceAppApp.swift
+//  FinalSpaceApp
+//
+//  Created by Bram Lippens on 02/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct FinalSpaceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
